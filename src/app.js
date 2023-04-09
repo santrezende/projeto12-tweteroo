af659567b17cd2a827472526347d1096e832c97c
santiago <santiagorsilva99@gmail.com>
2023-04-08 21:41:46 -0300

    validacao e status code

--- src/app.js
+++ src/app.js
@@ -7,26 +7,33 @@ const users = [];
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
+    if (!req.body.username || !req.body.avatar || typeof req.body.username !== "string" || typeof req.body.avatar !== "string") {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+    }
     users.push({
         username: req.body.username,
         avatar: req.body.avatar
     });
-    res.send("OK");
+    res.status(201).send("CREATED");
 });
 
 app.post("/tweets", (req, res) => {
-    const usernameTweet = users.find(user => user.username === req.body.username);
-    if (usernameTweet) {
-        const userAvatar = usernameTweet.avatar;
+    if (!req.body.username || !req.body.tweet || typeof req.body.username !== "string" || typeof req.body.tweet !== "string") {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+    }
+    const userTweet = users.find(user => user.username === req.body.username);
+    if (userTweet) {
+        const userAvatar = userTweet.avatar;
         tweets.push({
             username: req.body.username,
             avatar: userAvatar,
             tweet: req.body.tweet
         });
-        console.log(tweets);
-        res.send("OK");
+        res.status(201).send("CREATED");
     } else {
-        res.send("UNAUTHORIZED");
+        res.status(401).send("UNAUTHORIZED");
     };
 });
 
